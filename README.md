f73e949015446cb9fdbcd3cbb475faa12b329857
B1ker4nt3nd <marecsko.kornel@gmail.com>
2023-02-12 07:43:40 +0100

    init

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 https://medium.com/netscape/a-guide-to-create-a-nodejs-command-line-package-c2166ad0452e
 
-npm link
+- npm link
 
-npm unlink # No more command installed
+- npm unlink # No more command installed
+
+### Gyakorlatban
+
+Az `npm link` csinál valami npm install szerűt, és a user(`\Users\username\AppData\Roaming\npm\`) könyvtárba létrehoz egy linket ami utána globálisan elérhető mintha telepítettük volna a csomagot. A node_modules-ba linkeli a könyvátunkat.
+
+Az `npm unlink` nekem nem működik, nem tudom miért. Az npm unlink végülis lefut a `\Users\username\AppData\Roaming\npm\` könyvtáron amit nagyon nem szabad, mert mindent uninstall-ál. Linket a korábban már említett könyvtáron lehet kitörölni, és azzal megszűnik. Én amit próbáltam az `npm unlink <package_name>` vagy `npm uninstall <package_name>` ezek nem működtek. Ha az npm unlink lefut eredményesen akkor kiírja, hogy hány csomagot törölt.
--- cli.js
+++ cli.js
@@ -1,50 +1,51 @@
 #!/usr/bin/env node
-const [, , ...args] = process.argv;
-
-console.log(`Hello world ${args}`);
-
 // cli.js
 
-// const readline = require('readline');
-// const exec = require('child_process').exec;
-// const fs = require('fs');
+const readline = require('readline');
+const exec = require('child_process').exec;
+const fs = require('fs');
 
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-// rl.question('What should be the name of application? ', (answer) => {
-//   const appName = answer.trim();
-//   const folderName = appName.replace(/\s/g, '-');
+process.on('SIGINT', function () {
+  console.log('\nreceived SIGINT, goodbye!');
+  process.exit();
+});
 
-//   exec(
-//     `git clone https://github.com/your-github-repo.git ${folderName}`,
-//     (err, stdout, stderr) => {
-//       if (err) {
-//         console.error(`exec error: ${error}`);
-//         return;
-//       }
-//       console.log(`Git clone successful`);
-//       const packageJson = {
-//         name: appName,
-//       };
-//       fs.writeFile(
-//         `${folderName}/package.json`,
-//         JSON.stringify(packageJson, null, 2),
-//         (err) => {
-//           if (err) throw err;
-//           console.log('package.json successfully created');
-//           exec(`cd ${folderName} && npm install`, (err, stdout, stderr) => {
-//             if (err) {
-//               console.error(`exec error: ${error}`);
-//               return;
-//             }
-//             console.log(`Npm install successful`);
-//             process.exit();
-//           });
-//         }
-//       );
-//     }
-//   );
-// });
+rl.question('What should be the name of application? ', (answer) => {
+  const appName = answer.trim();
+  const folderName = appName.replace(/\s/g, '-');
+
+  exec(
+    `git clone https://github.com/B1ker4nt3nd/webpack-init.git ${folderName}`,
+    (err, stdout, stderr) => {
+      if (err) {
+        console.error(`exec error: ${error}`);
+        return;
+      }
+      console.log(`Git clone successful`);
+      const packageJson = {
+        name: appName,
+      };
+      fs.writeFile(
+        `${folderName}/package.json`,
+        JSON.stringify(packageJson, null, 2),
+        (err) => {
+          if (err) throw err;
+          console.log('package.json successfully created');
+          exec(`cd ${folderName} && npm install`, (err, stdout, stderr) => {
+            if (err) {
+              console.error(`exec error: ${error}`);
+              return;
+            }
+            console.log(`Npm install successful`);
+            process.exit();
+          });
+        }
+      );
+    }
+  );
+});
